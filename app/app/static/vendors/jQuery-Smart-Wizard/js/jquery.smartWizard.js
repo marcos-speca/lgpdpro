1beccd6895db17ce067668a12fd88f62bc37c1f4
Marcos Speca <marcos.speca@legalinsights.com.br>
2021-02-24 20:31:50 -0300

    Ajustes Gerais

--- app/app/static/vendors/jQuery-Smart-Wizard/js/jquery.smartWizard.js
+++ app/app/static/vendors/jQuery-Smart-Wizard/js/jquery.smartWizard.js
@@ -430,16 +430,16 @@ $.fn.smartWizard.defaults = {
     selected: 0,  // Selected Step, 0 = first step
     keyNavigation: true, // Enable/Disable key navigation(left and right keys are used if enabled)
     enableAllSteps: false,
-    transitionEffect: 'fade', // Effect on navigation, none/fade/slide/slideleft
+    transitionEffect: 'slide', // Effect on navigation, none/fade/slide/slideleft
     contentURL:null, // content url, Enables Ajax content loading
     contentCache:true, // cache step contents, if false content is fetched always from ajax url
     cycleSteps: false, // cycle step navigation
     enableFinishButton: false, // make finish button enabled always
 	hideButtonsOnDisabled: false, // when the previous/next/finish buttons are disabled, hide them instead?
     errorSteps:[],    // Array Steps with errors
-    labelNext:'Next',
-    labelPrevious:'Previous',
-    labelFinish:'Finish',
+    labelNext:'Próximo',
+    labelPrevious:'Anterior',
+    labelFinish:'Finalizar',
     noForwardJumping: false,
     onLeaveStep: null, // triggers when leaving a step
     onShowStep: null,  // triggers when showing a step
